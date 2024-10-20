86001297ee44802bf3929ed0bf50268549d5575b
Ranjithkumar <159144234+Ranjithk7@users.noreply.github.com>
2024-10-20 14:40:37 +0530

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialIcons = () => {
   return (
     <div className="social-icons">
-        <a href="https://wa.me/8838610066"><i className="fab fa-whatsapp"></i></a>
+        <a href="https://wa.me/918838610066"><i className="fab fa-whatsapp"></i></a>
         <a href="https://x.com/ranjithkum61241"><i className="fab fa-twitter"></i></a>
         <a href="https://www.instagram.com/ranjithk_03"><i className="fab fa-instagram"></i></a>
     </div>
